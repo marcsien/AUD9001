052c4f881f5d8df542e26e5a36e5c3e46f650ae0
marcsien <marcsien@gmail.com>
2021-12-01 22:10:32 +0100

    Prepare data access layer first step

---
+++ AUD9001/AUD9001.DataAccess/AUD9001StorageContext.cs
@@ -0,0 +1,20 @@
+﻿using AUD9001.DataAccess.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AUD9001.DataAccess
+{
+    class AUD9001StorageContext : DbContext
+    {
+        public AUD9001StorageContext(DbContextOptions<AUD9001StorageContext> opt ) : base(opt)
+        {
+
+        }
+
+        public DbSet<Process> Processes { get; set; }
+    }
+}
---
+++ AUD9001/AUD9001.DataAccess/Entities/Process.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AUD9001.DataAccess.Entities
+{
+    class Process
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string ProcessLiderId { get; set; }
+
+    }
+}
