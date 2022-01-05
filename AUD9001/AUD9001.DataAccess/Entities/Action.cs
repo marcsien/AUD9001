25e3f77c529a7e72f4da7c4d8c961c422222dfc1
marcsien <marcsien@gmail.com>
2022-01-05 08:07:55 +0100

    Add Action and RecommendedAction classes

---
+++ AUD9001/AUD9001.DataAccess/Entities/Action.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AUD9001.DataAccess.Entities
+{
+    public class Action  : EntityBase
+    {
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public string ResponsiblePersonId { get; set; }
+
+        public bool IsFinished { get; set; }
+    }
+}
---
+++ AUD9001/AUD9001.DataAccess/Entities/RecommendedAction.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AUD9001.DataAccess.Entities
+{
+    public class RecommendedAction : EntityBase 
+    {
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public string RecommendingPersonId { get; set; }
+
+        public bool IsFinished { get; set; }
+
+        public List<Action> Actions { get; set; }
+
+    }
+}
