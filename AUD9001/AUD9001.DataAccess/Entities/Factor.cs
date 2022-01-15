d0df1979441c6ea8e1dbc70d5c09a437fb4d2f8c
marcsien <marcsien@gmail.com>
2022-01-15 20:09:11 +0100

    Add Strategic Position Analysis and Factor classes

---
+++ AUD9001/AUD9001.DataAccess/Entities/Factor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AUD9001.DataAccess.Entities
+{
+    public class Factor : EntityBase
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int Importance { get; set; }
+        public bool Positive { get; set; }
+        public bool Internal { get; set; }
+        public StrategicPositionAnalysis StrategicPositionAnalysis { get; set; }
+    }
+}
---
+++ AUD9001/AUD9001.DataAccess/Entities/StrategicPositionAnalysis.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AUD9001.DataAccess.Entities
+{
+    public class StrategicPositionAnalysis : EntityBase
+    {
+        public DateTime CreationDate { get; set; }
+        public int RecordNumber { get; set; }
+        public int SummaryS { get; set; }
+        public int SummaryW { get; set; }
+        public int SummaryO { get; set; }
+        public int SummaryT { get; set; }
+        public int AR { get; set; }
+        public int PR { get; set; }
+        public Process Process { get; set; }
+    }
+}
