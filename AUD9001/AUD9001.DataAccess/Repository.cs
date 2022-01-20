c97f2c97da543cddf6e79cd9df83caa32eb58b2d
marcsien <marcsien@gmail.com>
2022-01-20 16:58:04 +0100

    Implement generic repository with DependencyInjection

---
+++ AUD9001/AUD9001.DataAccess/Repository.cs
@@ -0,0 +1,61 @@
+﻿using AUD9001.DataAccess.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AUD9001.DataAccess
+{
+    public class Repository<T> : IRepository<T> where T : EntityBase
+    {
+        protected readonly AUD9001StorageContext context;
+        private DbSet<T> entities;
+
+        public Repository(AUD9001StorageContext context)
+        {
+            this.context = context;
+            entities = context.Set<T>();
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            return entities.AsEnumerable();
+        }
+
+        public T GetById(int id)
+        {
+            return entities.SingleOrDefault(s => s.Id == id);
+        }
+
+        public void Insert(T entity)
+        {
+            if (entity == null)
+            {
+                throw new ArgumentNullException("entity");
+            }
+
+            entities.Add(entity);
+            context.SaveChanges();
+        }
+
+        public void Update(T entity)
+        {
+            if (entity == null)
+            {
+                throw new ArgumentNullException("entity");
+            }
+
+            entities.Update(entity);
+            context.SaveChanges();
+        }
+
+        public void Delete(int id)
+        {
+            T entity = entities.SingleOrDefault(s => s.Id == id);
+            entities.Remove(entity);
+            context.SaveChanges();
+        }
+    }
+}
--- AUD9001/AUD9001/Startup.cs
+++ AUD9001/AUD9001/Startup.cs
@@ -1,3 +1,4 @@
+using AUD9001.DataAccess;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -11,6 +12,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+
 
 namespace AUD9001
 {
@@ -26,6 +28,7 @@ namespace AUD9001
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddScoped(serviceType: typeof(IRepository<>), implementationType: typeof(Repository<>));
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
