9bf9f9ab734707b6d802ab396eb578e5aab68626
marcsien <marcsien@gmail.com>
2022-02-22 21:03:04 +0100

    Add first controller with method to get all processess and process by id.

---
+++ AUD9001/AUD9001.DataAccess/Migrations/20220222193841_TestingFirstController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AUD9001.DataAccess.Migrations
+{
+    public partial class TestingFirstController : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
---
+++ AUD9001/AUD9001/Controllers/ProcessController.cs
@@ -0,0 +1,29 @@
+﻿using AUD9001.DataAccess.Entities;
+using AUD9001.DataAccess;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AUD9001.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProcessController : ControllerBase
+    {
+        private readonly IRepository<Process> processRepository;
+        public ProcessController(IRepository<Process> processRepository)
+        {
+            this.processRepository = processRepository;
+        }
+
+        [HttpGet]
+        [Route("")]
+        public IEnumerable<Process> GetAllProcesses() => this.processRepository.GetAll();
+
+        [HttpGet]
+        [Route("{processId}")]
+        public Process GetProcessById(int processId) => this.processRepository.GetById(processId);
+    }
+}
