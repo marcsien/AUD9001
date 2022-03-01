343e999f0b877a7d179d896ed6c268cee81164c6
marcsien <marcsien@gmail.com>
2022-03-01 21:19:10 +0100

    Add getall() method for Users

---
+++ AUD9001/AUD9001.ApplicationServices/API/Domain/Models/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AUD9001.ApplicationServices.API.Domain.Models
+{
+    public class User
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Position { get; set; }
+        public string Email { get; set; }
+
+    }
+}
---
+++ AUD9001/AUD9001.ApplicationServices/API/Handlers/GetUsersHandler.cs
@@ -0,0 +1,42 @@
+﻿using AUD9001.ApplicationServices.API.Domain;
+using AUD9001.DataAccess;
+using AUD9001.DataAccess.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AUD9001.ApplicationServices.API.Handlers
+{
+    public class GetUsersHandler : IRequestHandler<GetUsersRequest, GetUsersResponse>
+    {
+        private readonly IRepository<User> userRepository;
+        public GetUsersHandler(IRepository<DataAccess.Entities.User> userRepository)
+        {
+            this.userRepository = userRepository;
+        }
+
+        public Task<GetUsersResponse> Handle(GetUsersRequest request, CancellationToken cancellationToken)
+        {
+            var users = this.userRepository.GetAll();
+
+            var domainUsers = users.Select(x => new Domain.Models.User()
+            {
+                Name = x.Name,
+                Surname = x.Surname,
+                Position = x.Position,
+                Email = x.Email
+            });
+
+            var response = new GetUsersResponse()
+            {
+                Data = domainUsers.ToList()
+            };
+
+            return Task.FromResult(response);
+        }
+    }
+}
