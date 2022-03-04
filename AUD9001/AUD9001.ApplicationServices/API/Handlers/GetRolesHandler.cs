f31b13937dfd9192c4538005798d60973673fde9
marcsien <marcsien@gmail.com>
2022-03-04 21:39:06 +0100

    Add GET request for Roles

---
+++ AUD9001/AUD9001.ApplicationServices/API/Handlers/GetRolesHandler.cs
@@ -0,0 +1,39 @@
+﻿using AUD9001.ApplicationServices.API.Domain;
+using AUD9001.DataAccess;
+using AUD9001.DataAccess.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AUD9001.ApplicationServices.API.Handlers
+{
+    public class GetRolesHandler : IRequestHandler<GetRolesRequest, GetRolesResponse>
+    {
+        private readonly IRepository<Role> roleRepository;
+        public GetRolesHandler(IRepository<DataAccess.Entities.Role> roleRepository)
+        {
+            this.roleRepository = roleRepository;
+        }
+        public Task<GetRolesResponse> Handle(GetRolesRequest request, CancellationToken cancellationToken)
+        {
+            var roles = this.roleRepository.GetAll();
+
+            var domainRoles = roles.Select(x => new Domain.Models.Role()
+            {
+                Name = x.Name,
+                Description = x.Description
+            });
+
+            var response = new GetRolesResponse()
+            {
+                Data = domainRoles.ToList()
+            };
+
+            return Task.FromResult(response);
+        }
+    }
+}
