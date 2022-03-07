10c20727dac6b33bc5c6fcafe093b4e2106991a2
marcsien <marcsien@gmail.com>
2022-03-07 18:35:13 +0100

    Add GET request for Resources

---
+++ AUD9001/AUD9001.ApplicationServices/API/Handlers/GetResourcesHandler.cs
@@ -0,0 +1,40 @@
+﻿using AUD9001.ApplicationServices.API.Domain;
+using AUD9001.DataAccess;
+using AUD9001.DataAccess.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AUD9001.ApplicationServices.API.Handlers
+{
+    public class GetResourcesHandler : IRequestHandler<GetResourcesRequest, GetResourcesResponse>
+    {
+        private readonly IRepository<Resource> resourceRepository;
+
+        public GetResourcesHandler(IRepository<Resource> resourceRepository)
+        {
+            this.resourceRepository = resourceRepository;
+        }
+
+        public Task<GetResourcesResponse> Handle(GetResourcesRequest request, CancellationToken cancellationToken)
+        {
+            var resources = this.resourceRepository.GetAll();
+
+            var domainResources = resources.Select(x => new Domain.Models.Resource()
+            {
+                Name = x.Name
+            });
+
+            var response = new GetResourcesResponse()
+            {
+                Data = domainResources.ToList()
+            };
+
+            return Task.FromResult(response);
+        }
+    }
+}
---
+++ AUD9001/AUD9001/Controllers/ResourcesController.cs
@@ -0,0 +1,32 @@
+﻿using AUD9001.DataAccess.Entities;
+using AUD9001.DataAccess;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MediatR;
+using AUD9001.ApplicationServices.API.Domain;
+
+namespace AUD9001.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ResourcesController : ControllerBase
+    {
+        private readonly IMediator mediator;
+
+        public ResourcesController(IMediator mediator)
+        {
+            this.mediator = mediator;
+        }
+
+        [HttpGet]
+        [Route("")]
+        public async Task<IActionResult> GetAllResources([FromQuery] GetResourcesRequest request)
+        {
+            var response = await this.mediator.Send(request);
+            return this.Ok(response);
+        }
+    }
+}
