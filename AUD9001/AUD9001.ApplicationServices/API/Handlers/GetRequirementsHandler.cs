ca064a77cb5dd730fcbe7eb768d21b71a94375bf
marcsien <marcsien@gmail.com>
2022-03-08 07:23:38 +0100

    Add GET request for Requirements

---
+++ AUD9001/AUD9001.ApplicationServices/API/Handlers/GetRequirementsHandler.cs
@@ -0,0 +1,41 @@
+﻿using AUD9001.ApplicationServices.API.Domain;
+using AUD9001.DataAccess;
+using AUD9001.DataAccess.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AUD9001.ApplicationServices.API.Handlers
+{
+    public class GetRequirementsHandler : IRequestHandler<GetRequirementsRequest, GetRequirementsResponse>
+    {
+        private readonly IRepository<Requirement> requirementRepository;
+
+        public GetRequirementsHandler(IRepository<Requirement> requirementRepository)
+        {
+            this.requirementRepository = requirementRepository;
+        }
+
+        public Task<GetRequirementsResponse> Handle(GetRequirementsRequest request, CancellationToken cancellationToken)
+        {
+            var requirements = this.requirementRepository.GetAll();
+
+            var domainRequirements = requirements.Select(x => new Domain.Models.Requirement()
+            {
+                Name = x.Name,
+                Description = x.Description
+            });
+
+            var response = new GetRequirementsResponse()
+            {
+                Data = domainRequirements.ToList()
+            };
+
+            return Task.FromResult(response);
+        }
+    }
+}
