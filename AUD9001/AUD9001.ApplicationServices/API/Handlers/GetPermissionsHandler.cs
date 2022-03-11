64a1cbed8401e3c52dd68049da379138f9e925da
marcsien <marcsien@gmail.com>
2022-03-11 20:39:54 +0100

    Add GET request for Permissions

---
+++ AUD9001/AUD9001.ApplicationServices/API/Handlers/GetPermissionsHandler.cs
@@ -0,0 +1,41 @@
+﻿using AUD9001.ApplicationServices.API.Domain;
+using AUD9001.DataAccess;
+using AUD9001.DataAccess.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AUD9001.ApplicationServices.API.Handlers
+{
+    public class GetPermissionsHandler : IRequestHandler<GetPermissionsRequest, GetPermissionsResponse>
+    {
+        private readonly IRepository<Permission> permissionRepository;
+
+        public GetPermissionsHandler(IRepository<Permission> permissionRepository)
+        {
+            this.permissionRepository = permissionRepository;
+        }
+
+        public Task<GetPermissionsResponse> Handle(GetPermissionsRequest request, CancellationToken cancellationToken)
+        {
+            var permissions = this.permissionRepository.GetAll();
+
+            var domainpermissions = permissions.Select(x => new Domain.Models.Permission()
+            {
+                Name = x.Name
+            });
+
+            var response = new GetPermissionsResponse()
+            {
+                Data = domainpermissions.ToList()
+            };
+
+            return Task.FromResult(response);
+        }
+    }
+
+}
