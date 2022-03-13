c2b20e367644a35975071a7bd11cac9cc6da5206
marcsien <marcsien@gmail.com>
2022-03-13 17:48:07 +0100

    Add GET request for Observations

---
+++ AUD9001/AUD9001.ApplicationServices/API/Handlers/GetObservationsHandler.cs
@@ -0,0 +1,41 @@
+﻿using AUD9001.ApplicationServices.API.Domain;
+using AUD9001.DataAccess;
+using AUD9001.DataAccess.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AUD9001.ApplicationServices.API.Handlers
+{
+    public class GetObservationsHandler : IRequestHandler<GetObservationsRequest, GetObservationsResponse>
+    {
+        private readonly IRepository<Observation> observationRepository;
+
+        public GetObservationsHandler(IRepository<DataAccess.Entities.Observation> observationRepository)
+        {
+            this.observationRepository = observationRepository;
+        }
+
+        public Task<GetObservationsResponse> Handle(GetObservationsRequest request, CancellationToken cancellationToken)
+        {
+            var observations = this.observationRepository.GetAll();
+
+            var domainObservations = observations.Select(x => new Domain.Models.Observation()
+            {
+                Name = x.Name,
+                Description = x.Description
+            });
+
+            var response = new GetObservationsResponse()
+            {
+                Data = domainObservations.ToList()
+            };
+
+            return Task.FromResult(response);
+        }
+    }
+}
