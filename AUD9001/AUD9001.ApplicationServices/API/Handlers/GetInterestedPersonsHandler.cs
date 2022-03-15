a27920bff6d4c265b6b703b203892185866a62a2
marcsien <marcsien@gmail.com>
2022-03-15 18:43:53 +0100

    Add GET request for InterestedPersons

---
+++ AUD9001/AUD9001.ApplicationServices/API/Handlers/GetInterestedPersonsHandler.cs
@@ -0,0 +1,41 @@
+﻿using AUD9001.ApplicationServices.API.Domain;
+using AUD9001.DataAccess;
+using AUD9001.DataAccess.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AUD9001.ApplicationServices.API.Handlers
+{
+    public class GetInterestedPersonsHandler : IRequestHandler<GetInterestedPersonsRequest, GetInterestedPersonsResponse>
+    {
+        private readonly IRepository<InterestedPerson> interestedPersonRepository;
+
+        public GetInterestedPersonsHandler(IRepository<DataAccess.Entities.InterestedPerson> interestedPersonRepository)
+        {
+            this.interestedPersonRepository = interestedPersonRepository;
+        }
+
+        public Task<GetInterestedPersonsResponse> Handle(GetInterestedPersonsRequest request, CancellationToken cancellationToken)
+        {
+            var interestedPersons = this.interestedPersonRepository.GetAll();
+
+            var domainInterestedPersons = interestedPersons.Select(x => new Domain.Models.InterestedPerson()
+            {
+                Name = x.Name,
+                Description = x.Description
+            });
+
+            var response = new GetInterestedPersonsResponse()
+            {
+                Data = domainInterestedPersons.ToList()
+            };
+
+            return Task.FromResult(response);
+        }
+    }
+}
