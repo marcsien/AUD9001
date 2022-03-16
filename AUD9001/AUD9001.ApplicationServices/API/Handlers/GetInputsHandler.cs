95eaadfe14532cfc9a2050bdc6cb449a8e4a725c
marcsien <marcsien@gmail.com>
2022-03-16 18:31:46 +0100

    Add GET request for Inputs

---
+++ AUD9001/AUD9001.ApplicationServices/API/Handlers/GetInputsHandler.cs
@@ -0,0 +1,41 @@
+﻿using AUD9001.ApplicationServices.API.Domain;
+using AUD9001.DataAccess;
+using AUD9001.DataAccess.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AUD9001.ApplicationServices.API.Handlers
+{
+    public class GetInputsHandler : IRequestHandler<GetInputsRequest, GetInputsResponse>
+    {
+        private readonly IRepository<Input> inputRepository;
+
+        public GetInputsHandler(IRepository<DataAccess.Entities.Input> inputRepository)
+        {
+            this.inputRepository = inputRepository;
+        }
+
+        public Task<GetInputsResponse> Handle(GetInputsRequest request, CancellationToken cancellationToken)
+        {
+            var inputs = this.inputRepository.GetAll();
+
+            var domainInputs = inputs.Select(x => new Domain.Models.Input()
+            {
+                Name = x.Name,
+                Description = x.Description
+            });
+
+            var response = new GetInputsResponse()
+            {
+                Data = domainInputs.ToList()
+            };
+
+            return Task.FromResult(response);
+        }
+    }
+}
---
+++ AUD9001/AUD9001/Controllers/InputsController.cs
@@ -0,0 +1,31 @@
+﻿using AUD9001.DataAccess.Entities;
+using AUD9001.DataAccess;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MediatR;
+using AUD9001.ApplicationServices.API.Domain;
+
+namespace AUD9001.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class InputsController : ControllerBase
+    {
+        private readonly IMediator mediator;
+        public InputsController(IMediator mediator)
+        {
+            this.mediator = mediator;
+        }
+
+        [HttpGet]
+        [Route("")]
+        public async Task<IActionResult> GetAllInputs([FromQuery] GetInputsRequest request)
+        {
+            var response = await this.mediator.Send(request);
+            return this.Ok(response);
+        }
+    }
+}
