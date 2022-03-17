db385443e202669e9b0b2326038be8fff3df9aa4
marcsien <marcsien@gmail.com>
2022-03-17 07:34:26 +0100

    Add GET request for Inconsistencies

---
+++ AUD9001/AUD9001.ApplicationServices/API/Handlers/GetInconsistenciesHandler.cs
@@ -0,0 +1,41 @@
+﻿using AUD9001.ApplicationServices.API.Domain;
+using AUD9001.DataAccess;
+using AUD9001.DataAccess.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AUD9001.ApplicationServices.API.Handlers
+{
+    public class GetInconsistenciesHandler : IRequestHandler<GetInconsistenciesRequest, GetInconsistenciesResponse>
+    {
+        private readonly IRepository<Inconsistency> inconsistencyRepository;
+
+        public GetInconsistenciesHandler(IRepository<DataAccess.Entities.Inconsistency> inconsistencyRepository)
+        {
+            this.inconsistencyRepository = inconsistencyRepository;
+        }
+
+        public Task<GetInconsistenciesResponse> Handle(GetInconsistenciesRequest request, CancellationToken cancellationToken)
+        {
+            var inconsistencies = this.inconsistencyRepository.GetAll();
+
+            var domainInconsistencies = inconsistencies.Select(x => new Domain.Models.Inconsistency()
+            {
+                Name = x.Name,
+                Description = x.Description
+            });
+
+            var response = new GetInconsistenciesResponse()
+            {
+                Data = domainInconsistencies.ToList()
+            };
+
+            return Task.FromResult(response);
+        }
+    }
+}
