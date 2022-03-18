57add610424a6dd363ff5a038b729eb79760e735
marcsien <marcsien@gmail.com>
2022-03-18 07:38:19 +0100

    Add GET request for Factors + Update appsettings.json with new database server name

---
+++ AUD9001/AUD9001.ApplicationServices/API/Handlers/GetFactorsHandler.cs
@@ -0,0 +1,41 @@
+﻿using AUD9001.ApplicationServices.API.Domain;
+using AUD9001.DataAccess;
+using AUD9001.DataAccess.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AUD9001.ApplicationServices.API.Handlers
+{
+    public class GetFactorsHandler : IRequestHandler<GetFactorsRequest, GetFactorsResponse>
+    {
+        private readonly IRepository<Factor> factorRepository;
+
+        public GetFactorsHandler(IRepository<DataAccess.Entities.Factor> factorRepository)
+        {
+            this.factorRepository = factorRepository;
+        }
+
+        public Task<GetFactorsResponse> Handle(GetFactorsRequest request, CancellationToken cancellationToken)
+        {
+            var factors = this.factorRepository.GetAll();
+
+            var domainFactors = factors.Select(x => new Domain.Models.Factor()
+            {
+                Name = x.Name,
+                Description = x.Description
+            });
+
+            var response = new GetFactorsResponse()
+            {
+                Data = domainFactors.ToList()
+            };
+
+            return Task.FromResult(response);
+        }
+    }
+}
