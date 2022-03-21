15c7534f0d369130f7f2fb7c8e9721b901c8708f
marcsien <marcsien@gmail.com>
2022-03-21 19:34:33 +0100

    Add GET request for Expectations

---
+++ AUD9001/AUD9001.ApplicationServices/API/Handlers/GetExpectationsHandler.cs
@@ -0,0 +1,41 @@
+﻿using AUD9001.ApplicationServices.API.Domain;
+using AUD9001.DataAccess;
+using AUD9001.DataAccess.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AUD9001.ApplicationServices.API.Handlers
+{
+    public class GetExpectationsHandler : IRequestHandler<GetExpectationsRequest, GetExpectationsResponse>
+    {
+        private readonly IRepository<Expectation> expectationRepository;
+
+        public GetExpectationsHandler(IRepository<DataAccess.Entities.Expectation> expectationRepository)
+        {
+            this.expectationRepository = expectationRepository;
+        }
+
+        public Task<GetExpectationsResponse> Handle(GetExpectationsRequest request, CancellationToken cancellationToken)
+        {
+            var expectations = this.expectationRepository.GetAll();
+
+            var domainExpectations = expectations.Select(x => new Domain.Models.Expectation()
+            {
+                Name = x.Name,
+                Description = x.Description
+            });
+
+            var response = new GetExpectationsResponse()
+            {
+                Data = domainExpectations.ToList()
+            };
+
+            return Task.FromResult(response);
+        }
+    }
+}
