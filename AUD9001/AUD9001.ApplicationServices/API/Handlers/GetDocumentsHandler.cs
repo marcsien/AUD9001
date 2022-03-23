2cc97d57aac876006796ddb6dcf6b4206ce111ca
marcsien <marcsien@gmail.com>
2022-03-23 18:37:17 +0100

    Add GET request for Documents and delete unused DiagramOfEntities

---
+++ AUD9001/AUD9001.ApplicationServices/API/Handlers/GetDocumentsHandler.cs
@@ -0,0 +1,41 @@
+﻿using AUD9001.ApplicationServices.API.Domain;
+using AUD9001.DataAccess;
+using AUD9001.DataAccess.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AUD9001.ApplicationServices.API.Handlers
+{
+    class GetDocumentsHandler : IRequestHandler<GetDocumentsRequest, GetDocumentsResponse>
+    {
+        private readonly IRepository<DataAccess.Entities.Document> documentRepository;
+
+        public GetDocumentsHandler(IRepository<Document> documentRepository)
+        {
+            this.documentRepository = documentRepository;
+        }
+
+        public Task<GetDocumentsResponse> Handle(GetDocumentsRequest request, CancellationToken cancellationToken)
+        {
+            var documents = this.documentRepository.GetAll();
+
+            var domainDocuments = documents.Select(x => new Domain.Models.Document()
+            {
+                Name = x.Name,
+                Description = x.Description
+            });
+
+            var response = new GetDocumentsResponse()
+            {
+                Data = domainDocuments.ToList()
+            };
+
+            return Task.FromResult(response);
+        }
+    }
+}
