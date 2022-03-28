cab0e0a43c90b797b6c79dcbed28edc1ac3b14f2
marcsien <marcsien@gmail.com>
2022-03-28 18:47:53 +0200

    Add GET request for Actions

---
+++ AUD9001/AUD9001.ApplicationServices/API/Handlers/GetActionsHandler.cs
@@ -0,0 +1,41 @@
+﻿using AUD9001.ApplicationServices.API.Domain;
+using AUD9001.DataAccess;
+using AUD9001.DataAccess.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AUD9001.ApplicationServices.API.Handlers
+{
+    public class GetActionsHandler : IRequestHandler<GetActionsRequest, GetActionsResponse>
+    {
+        private readonly IRepository<DataAccess.Entities.Action> actionRepository;
+
+        public GetActionsHandler(IRepository<DataAccess.Entities.Action> actionRepository)
+        {
+            this.actionRepository = actionRepository;
+        }
+
+        public Task<GetActionsResponse> Handle(GetActionsRequest request, CancellationToken cancellationToken)
+        {
+            var actions = this.actionRepository.GetAll();
+
+            var domainActions = actions.Select(x => new Domain.Models.Action()
+            {
+                Name = x.Name,
+                Description = x.Description
+            });
+
+            var response = new GetActionsResponse()
+            {
+                Data = domainActions.ToList()
+            };
+
+            return Task.FromResult(response);
+        }
+    }
+}
