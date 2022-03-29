e9f24e6db04b24d0e95ca784547196fb877e04fd
marcsien <marcsien@gmail.com>
2022-03-29 19:15:56 +0200

    Add GET request for Processes using Mediator

---
+++ AUD9001/AUD9001.ApplicationServices/API/Handlers/GetProcessesHandler.cs
@@ -0,0 +1,42 @@
+﻿using AUD9001.ApplicationServices.API.Domain;
+using AUD9001.DataAccess;
+using AUD9001.DataAccess.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AUD9001.ApplicationServices.API.Handlers
+{
+    public class GetProcessesHandler : IRequestHandler<GetProcessesRequest, GetProcessesResponse>
+    {
+        private readonly IRepository<Process> processRepository;
+
+        public GetProcessesHandler(IRepository<DataAccess.Entities.Process> processRepository)
+        {
+            this.processRepository = processRepository;
+        }
+
+        public Task<GetProcessesResponse> Handle(GetProcessesRequest request, CancellationToken cancellationToken)
+        {
+            var processes = this.processRepository.GetAll();
+
+            var domainProcesses = processes.Select(x => new Domain.Models.Process()
+            {
+                Id = x.Id,
+                Name = x.Name,
+                Description = x.Description
+            });
+
+            var response = new GetProcessesResponse()
+            {
+                Data = domainProcesses.ToList()
+            };
+
+            return Task.FromResult(response);
+        }
+    }
+}
--- AUD9001/AUD9001/Controllers/ProcessesController.cs
+++ AUD9001/AUD9001/Controllers/ProcessesController.cs
@@ -5,6 +5,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using MediatR;
+using AUD9001.ApplicationServices.API.Domain;
 
 namespace AUD9001.Controllers
 {
@@ -12,18 +14,47 @@ namespace AUD9001.Controllers
     [Route("[controller]")]
     public class ProcessesController : ControllerBase
     {
-        private readonly IRepository<Process> processRepository;
-        public ProcessesController(IRepository<Process> processRepository)
+        private readonly IMediator mediator;
+
+        public ProcessesController(IMediator mediator)
         {
-            this.processRepository = processRepository;
+            this.mediator = mediator;
         }
 
         [HttpGet]
         [Route("")]
-        public IEnumerable<Process> GetAllProcesses() => this.processRepository.GetAll();
+        public async Task<IActionResult> GetAllProcesses([FromQuery] GetProcessesRequest request)
+        {
+            var response = await this.mediator.Send(request);
+            return this.Ok(response);
+        }
 
+        //below part to furhter refinement
         [HttpGet]
         [Route("{processId}")]
-        public Process GetProcessById(int processId) => this.processRepository.GetById(processId);
+        public async Task<IActionResult> GetProcessById([FromQuery] GetProcessesRequest request)
+        {
+            var response = await this.mediator.Send(request);
+            return this.Ok(response);
+        }
     }
+    //oldversion
+    //[ApiController]
+    //[Route("[controller]")]
+    //public class ProcessesController : ControllerBase
+    //{
+    //    private readonly IRepository<Process> processRepository;
+    //    public ProcessesController(IRepository<Process> processRepository)
+    //    {
+    //        this.processRepository = processRepository;
+    //    }
+
+    //    [HttpGet]
+    //    [Route("")]
+    //    public IEnumerable<Process> GetAllProcesses() => this.processRepository.GetAll();
+
+    //    [HttpGet]
+    //    [Route("{processId}")]
+    //    public Process GetProcessById(int processId) => this.processRepository.GetById(processId);
+    //}
 }
