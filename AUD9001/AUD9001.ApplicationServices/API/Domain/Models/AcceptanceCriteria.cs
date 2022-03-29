fcad8b94b2f0f655e2895e9ea4c700b3ab531611
marcsien <marcsien@gmail.com>
2022-03-29 18:28:05 +0200

    Add GET request for AcceptanceCriterias

---
+++ AUD9001/AUD9001.ApplicationServices/API/Domain/Models/AcceptanceCriteria.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AUD9001.ApplicationServices.API.Domain.Models
+{
+    public class AcceptanceCriteria
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ AUD9001/AUD9001.ApplicationServices/API/Handlers/GetAcceptanceCriteriasHandler.cs
@@ -0,0 +1,41 @@
+﻿using AUD9001.ApplicationServices.API.Domain;
+using AUD9001.DataAccess;
+using AUD9001.DataAccess.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AUD9001.ApplicationServices.API.Handlers
+{
+    public class GetAcceptanceCriteriasHandler : IRequestHandler<GetAcceptanceCriteriasRequest, GetAcceptanceCriteriasResponse>
+    {
+        private readonly IRepository<AcceptanceCriteria> acceptnceCriteriasRepository;
+
+        public GetAcceptanceCriteriasHandler(IRepository<DataAccess.Entities.AcceptanceCriteria> acceptnceCriteriasRepository)
+        {
+            this.acceptnceCriteriasRepository = acceptnceCriteriasRepository;
+        }
+
+        public Task<GetAcceptanceCriteriasResponse> Handle(GetAcceptanceCriteriasRequest request, CancellationToken cancellationToken)
+        {
+            var acceptanceCriterias = this.acceptnceCriteriasRepository.GetAll();
+
+            var domainAcceptnceCriterias = acceptanceCriterias.Select(x => new Domain.Models.AcceptanceCriteria()
+            {
+                Name = x.Name,
+                Description = x.Description
+            });
+
+            var response = new GetAcceptanceCriteriasResponse()
+            {
+                Data = domainAcceptnceCriterias.ToList()
+            };
+
+            return Task.FromResult(response);
+        }
+    }
+}
