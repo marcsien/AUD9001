e53e4bcefe365bc79159f7cb89e887ee639b8fe0
marcsien <marcsien@gmail.com>
2022-05-26 06:58:23 +0200

    Add unit test project.

---
+++ AUD9001/AUD9001.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AUD9001.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
