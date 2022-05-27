0a35913f16843354545afb0fa93b61f47c5ef8b8
marcsien <marcsien@gmail.com>
2022-05-27 07:47:33 +0200

    Add unit tests templates

---
+++ AUD9001/AUD9001.Tests/HasherTests.cs
@@ -0,0 +1,20 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AUD9001.Tests
+{
+    [TestClass]
+    public class HasherTests
+    {
+        [TestMethod]
+        public void GenerateHash_ProperParameters_ReturnsHash()
+        {
+            Assert.Inconclusive();
+        }
+
+        [TestMethod]
+        public void GenerateSalt_ProperParameters_ReturnsSalt()
+        {
+            Assert.Inconclusive();
+        }
+    }
+}
