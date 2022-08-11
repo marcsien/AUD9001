30b28c8d660a75c1236c83e29cc399b093dff3c5
marcsien <marcsien@gmail.com>
2022-08-11 08:26:00 +0200

    Update AddProcessRequestValidator

--- AUD9001/AUD9001.ApplicationServices/API/Validators/AddProcessRequestValidator.cs
+++ AUD9001/AUD9001.ApplicationServices/API/Validators/AddProcessRequestValidator.cs
@@ -13,6 +13,8 @@ namespace AUD9001.ApplicationServices.API.Validators
         public AddProcessRequestValidator()
         {
             this.RuleFor(x => x.Name).NotEmpty().WithMessage("Name cannot be empty");
+            this.RuleFor(x => x.CompanyID).NotEmpty().WithMessage("CompanyID cannot be empty");
+            this.RuleFor(x => x.CompanyID).NotEqual(0).WithMessage("CompanyID cannot equal 0");
         }
     }
 }
