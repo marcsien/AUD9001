ceb26a6f2f973454b276debd4b20d3980b6abbd4
marcsien <marcsien@gmail.com>
2022-08-25 07:51:11 +0200

    AddOutputRequest Validation

---
+++ AUD9001/AUD9001.ApplicationServices/API/Validators/AddOutputRequestValidator.cs
@@ -0,0 +1,19 @@
+﻿using AUD9001.ApplicationServices.API.Domain.Output;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AUD9001.ApplicationServices.API.Validators
+{
+    public class AddOutputRequestValidator : AbstractValidator<AddOutputRequest>
+    {
+        public AddOutputRequestValidator()
+        {
+            this.RuleFor(x => x.Name).NotEmpty().WithMessage("Name cannot be empty");
+            this.RuleFor(x => x.ProcessID).NotEmpty().WithMessage("ProcessID cannot be empty");
+        }
+    }
+}
