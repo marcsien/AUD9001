bde39cfd1cd254b50ca20f5ad762d541b6195ccb
marcsien <marcsien@gmail.com>
2022-08-30 07:48:26 +0200

    Add Validators for GET Output requests

---
+++ AUD9001/AUD9001.ApplicationServices/API/Validators/GetOutputByIdRequestValidator.cs
@@ -0,0 +1,20 @@
+﻿using AUD9001.ApplicationServices.API.Domain.Output;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace AUD9001.ApplicationServices.API.Validators
+{
+    public class GetOutputByIdRequestValidator : AbstractValidator<GetOutputByIdRequest>
+    {
+        public GetOutputByIdRequestValidator()
+        {
+            this.RuleFor(x => x.OutputId).NotEmpty().WithMessage("ProcessId cannot be empty");
+            this.RuleFor(x => x.OutputId).NotEqual(0).WithMessage("ProcessId cannot equal 0");
+        }
+    }
+}
