e9e1b1613e1cbc9f4b156d0af615f2d388aaff9b
marcsien <marcsien@gmail.com>
2022-09-08 07:31:15 +0200

    AddCompanyRequest Validation

---
+++ AUD9001/AUD9001.ApplicationServices/API/Validators/AddCompanyRequestValidator.cs
@@ -0,0 +1,18 @@
+﻿using AUD9001.ApplicationServices.API.Domain.Company;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AUD9001.ApplicationServices.API.Validators
+{
+    public class AddCompanyRequestValidator : AbstractValidator<AddCompanyRequest>
+    {
+        public AddCompanyRequestValidator()
+        {
+            this.RuleFor(x => x.Name).NotEmpty().WithMessage("Name cannot be empty");
+        }
+    }
+}
