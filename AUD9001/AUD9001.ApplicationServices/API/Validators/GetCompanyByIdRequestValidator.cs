e2112b0011f0a52abcde4b6a630dfb09ea6e8a22
marcsien <marcsien@gmail.com>
2022-09-09 07:51:32 +0200

    Get Requests validators for Companies

---
+++ AUD9001/AUD9001.ApplicationServices/API/Validators/GetCompanyByIdRequestValidator.cs
@@ -0,0 +1,20 @@
+﻿using AUD9001.ApplicationServices.API.Domain.Company;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace AUD9001.ApplicationServices.API.Validators
+{
+    public class GetCompanyByIdRequestValidator : AbstractValidator<GetCompanyByIdRequest>
+    {
+        public GetCompanyByIdRequestValidator()
+        {
+            this.RuleFor(x => x.CompanyId).NotEmpty().WithMessage("CompanyId cannot be empty");
+            this.RuleFor(x => x.CompanyId).NotEqual(0).WithMessage("CompanyId cannot equal 0");
+        }
+    }
+}
