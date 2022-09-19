bf5eb6f5718767bb299bfff3f825663f59f0248d
marcsien <marcsien@gmail.com>
2022-09-19 07:13:54 +0200

    AddManagementReviewRequestValidator

---
+++ AUD9001/AUD9001.ApplicationServices/API/Validators/AddManagementReviewRequestValidator.cs
@@ -0,0 +1,18 @@
+﻿using AUD9001.ApplicationServices.API.Domain.ManagementReview;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AUD9001.ApplicationServices.API.Validators
+{
+    public class AddManagementReviewRequestValidator : AbstractValidator<AddManagementReviewRequest>
+    {
+        public AddManagementReviewRequestValidator()
+        {
+            this.RuleFor(x => x.CompanyID).NotEmpty().WithMessage("CompanyID cannot be empty");
+        }
+    }
+}
