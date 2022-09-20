61b08324405c3e311f81ab9b9261953453391a2a
marcsien <marcsien@gmail.com>
2022-09-20 08:04:25 +0200

    GetManagementReviewByIdRequestValidator + GetManagementReviewsRequestValidator

---
+++ AUD9001/AUD9001.ApplicationServices/API/Validators/GetManagementReviewByIdRequestValidator.cs
@@ -0,0 +1,20 @@
+﻿using AUD9001.ApplicationServices.API.Domain.ManagementReview;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace AUD9001.ApplicationServices.API.Validators
+{
+    public class GetManagementReviewByIdRequestValidator : AbstractValidator<GetManagementReviewByIdRequest>
+    {
+        public GetManagementReviewByIdRequestValidator()
+        {
+            this.RuleFor(x => x.ManagementReviewId).NotEmpty().WithMessage("ManagementReviewId cannot be empty");
+            this.RuleFor(x => x.ManagementReviewId).NotEqual(0).WithMessage("ManagementReviewId cannot equal 0");
+        }
+    }
+}
---
+++ AUD9001/AUD9001.ApplicationServices/API/Validators/GetManagementReviewsRequestValidator.cs
@@ -0,0 +1,18 @@
+﻿using AUD9001.ApplicationServices.API.Domain;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace AUD9001.ApplicationServices.API.Validators
+{
+    public class GetManagementReviewsRequestValidator : AbstractValidator<GetManagementReviewsRequest>
+    {
+        public GetManagementReviewsRequestValidator()
+        {
+        }
+    }
+}
