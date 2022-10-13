385a4bde7fd651582761551c47f184629fc4bc2d
marcsien <marcsien@gmail.com>
2022-10-13 07:30:40 +0200

    Error handling in GetCompaniesHandler

--- AUD9001/AUD9001.ApplicationServices/API/Handlers/GetCompaniesHandler.cs
+++ AUD9001/AUD9001.ApplicationServices/API/Handlers/GetCompaniesHandler.cs
@@ -1,4 +1,5 @@
 ﻿using AUD9001.ApplicationServices.API.Domain;
+using AUD9001.ApplicationServices.API.ErrorHandling;
 using AUD9001.DataAccess;
 using AUD9001.DataAccess.CQRS.Queries;
 using AUD9001.DataAccess.Entities;
@@ -15,25 +16,26 @@ namespace AUD9001.ApplicationServices.API.Handlers
 {
     public class GetCompaniesHandler : IRequestHandler<GetCompaniesRequest, GetCompaniesResponse>
     {
-        //private readonly IRepository<Company> companyRepository; IRepository<DataAccess.Entities.Company> companyRepository,
         private readonly IMapper mapper;
         private readonly IQueryExecutor queryexecutor;
 
         public GetCompaniesHandler(IMapper mapper, IQueryExecutor executor)
         {
-            //this.companyRepository = companyRepository;
             this.mapper = mapper;
             this.queryexecutor = executor;
         }
 
         public async Task<GetCompaniesResponse> Handle(GetCompaniesRequest request, CancellationToken cancellationToken)
         {
-            //var mappedCompanies = this.mapper.Map<List<Domain.Models.Company>>(await this.companyRepository.GetAll());
-            //var response = new GetCompaniesResponse() { Data = mappedCompanies };
-            //return response;
-
             var query = new GetCompaniesQuery();
             var companies = await this.queryexecutor.Execute(query);
+            if (companies == null)
+            {
+                return new GetCompaniesResponse()
+                {
+                    Error = new ErrorModel(ErrorType.NotFound)
+                };
+            }
             var mappedCompanies = this.mapper.Map<List<Domain.Models.Company>>(companies);
             var response = new GetCompaniesResponse() { Data = mappedCompanies };
             return response;
